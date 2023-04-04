6178283d9a5cc936a72a1e9d47a5c7e32d09f202
Anders Jacobsson <anders.jacobsson@columbusglobal.com>
2023-04-04 20:03:41 +0200

    Fixed problem with URL encoding

--- src/main/java/nu/fgv/register/migration/reader/AbstractReader.java
+++ src/main/java/nu/fgv/register/migration/reader/AbstractReader.java
@@ -2,12 +2,19 @@ package nu.fgv.register.migration.reader;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 public class AbstractReader {
 
     protected final JdbcTemplate jdbcTemplate;
 
     protected AbstractReader(final JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    static String encodeUrl(final String url) {
+        return URLEncoder.encode(url, StandardCharsets.UTF_8);
     }
 
 }
